5077242236f5e5e08d18c0227dd12eb8018120a1
Fatih Sevban KARTAL <87607332+fsk29@users.noreply.github.com>
2022-03-29 12:02:42 +0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,26 @@
+
+      let isimalani=document.querySelector("#myName");
+      var isim= prompt("Adınızı Girin");
+      if(isim.length>0){
+      isimalani.innerHTML=isim;
+      }
+      else{
+      isimalani.innerHTML="Misafir";
+      }
+  
+    
+    
+    function showTime(){
+      
+      var tarih= new Date();
+      var gun=tarih.getDay();
+        var saat=tarih.getHours();
+        var dakika=tarih.getMinutes();
+        var saniye=tarih.getSeconds();
+      let gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];	
+      document.getElementById("myClock").innerHTML=saat+":"+dakika+":"+saniye+" "+ gunler[gun];
+      setTimeout(showTime, 1000); 
+     
+  }
+
+  showTime();
